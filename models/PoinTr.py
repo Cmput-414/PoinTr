d69c12028ab7cf9d472a59f81a0868e83ffa9523
jackie174 <57372321+jackie174@users.noreply.github.com>
2022-12-03 16:09:13 -0700

    Update PoinTr.py

--- models/PoinTr.py
+++ models/PoinTr.py
@@ -43,12 +43,12 @@ class Fold(nn.Module):
             nn.ReLU(inplace=True),
             nn.Conv1d(hidden_dim//2, 3, 1),
         )
-        self.second_conv = nn.Sequential(
-            nn.Conv1d(512,512,1),
-            nn.BatchNorm1d(512),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(512,self.encoder_channel,1)
-        )
+        # self.second_conv = nn.Sequential(
+        #     nn.Conv1d(512,512,1),
+        #     nn.BatchNorm1d(512),
+        #     nn.ReLU(inplace=True),
+        #     nn.Conv1d(512,self.encoder_channel,1)
+        # )
 
     def forward(self, x):
         print("-"*20, x.size())
