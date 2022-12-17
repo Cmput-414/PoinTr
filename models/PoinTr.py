a731c39905ac724943b2764fef3e8f6dd0391590
jackie174 <57372321+jackie174@users.noreply.github.com>
2022-12-17 00:38:41 -0700

    change pointr

--- models/PoinTr.py
+++ models/PoinTr.py
@@ -12,6 +12,47 @@ def fps(pc, num):
     sub_pc = pointnet2_utils.gather_operation(pc.transpose(1, 2).contiguous(), fps_idx).transpose(1,2).contiguous()
     return sub_pc
 
+class LBCNN(nn.Module):
+    """
+    This class is used to create label vector for each partial model.
+    fd[1] and fd[2] are result from folding net.
+    Input: partial point cloud (total_batch size, number of query, fd[1]xfd[2] )
+    Output: Array of label
+    """
+    def __init__(self, in_channel, hidden_dim = 512):
+        super().__init__()
+
+        self.in_channel = in_channel
+        self.Cls1 = nn.Sequential(
+            nn.Conv1d(224, 64, 3, 1, 1),
+            nn.BatchNorm1d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(3, stride=2),
+            nn.Conv1d(64, 32, 3, 1, 1),
+            nn.BatchNorm1d(32),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(3, stride=2),
+            nn.Conv1d(32, 16, 3, 1, 1),
+            nn.BatchNorm1d(16),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(3, stride=2),
+            nn.Conv1d(16, 8, 3, 1, 1),
+            nn.BatchNorm1d(8),
+            nn.ReLU(inplace=True),
+            nn.Flatten(),
+        )
+        self.fc1 = nn.Linear(184, 64)
+        self.dropout = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(64, 8)
+        self.sf = nn.Softmax()
+        
+    def forward(self, x):
+        label1 = self.Cls1(x) #16, 760
+        label1 = self.fc1(label1) #16, 64
+        label1 = self.dropout(label1)
+        label1 = self.fc2(label1) #16, 8
+
+        return label1
 
 class Fold(nn.Module):
     def __init__(self, in_channel , step , hidden_dim = 512):
@@ -44,7 +85,9 @@ class Fold(nn.Module):
             nn.Conv1d(hidden_dim//2, 3, 1),
         )
 
+
     def forward(self, x):
+        #print(x.size()) #BM S.  Total_bs * num_Query, Token
         num_sample = self.step * self.step
         bs = x.size(0)
         features = x.view(bs, self.in_channel, 1).expand(bs, self.in_channel, num_sample)
@@ -53,8 +96,13 @@ class Fold(nn.Module):
         x = torch.cat([seed, features], dim=1)
         fd1 = self.folding1(x)
         x = torch.cat([fd1, features], dim=1)
-        fd2 = self.folding2(x)
+        fd2 = self.folding2(x) #3584, 3, 64
 
+
+        
+        #print(label1.size())
+        #print(fd2.size())
+        #print(fd2.size()) 
         return fd2
 
 @MODELS.register_module()
@@ -70,6 +118,7 @@ class PoinTr(nn.Module):
         self.base_model = PCTransformer(in_chans = 3, embed_dim = self.trans_dim, depth = [6, 8], drop_rate = 0., num_query = self.num_query, knn_layer = self.knn_layer)
         
         self.foldingnet = Fold(self.trans_dim, step = self.fold_step, hidden_dim = 256)  # rebuild a cluster point
+        self.CNN = LBCNN(self.trans_dim, hidden_dim = 256)
 
         self.increase_dim = nn.Sequential(
             nn.Conv1d(self.trans_dim, 1024, 1),
@@ -82,11 +131,13 @@ class PoinTr(nn.Module):
 
     def build_loss_func(self):
         self.loss_func = ChamferDistanceL1()
+        self.C_E_loss_func = nn.CrossEntropyLoss()
 
-    def get_loss(self, ret, gt):
+    def get_loss(self, ret, gt, vector):
         loss_coarse = self.loss_func(ret[0], gt)
         loss_fine = self.loss_func(ret[1], gt)
-        return loss_coarse, loss_fine
+        loss_Cls = self.C_E_loss_func(ret[2], vector.long())
+        return loss_coarse, loss_fine, loss_Cls
 
     def forward(self, xyz):
         q, coarse_point_cloud = self.base_model(xyz) # B M C and B M 3
@@ -106,7 +157,12 @@ class PoinTr(nn.Module):
         # coarse_point_cloud = self.refine_coarse(rebuild_feature).reshape(B, M, 3)
 
         # NOTE: foldingNet
-        relative_xyz = self.foldingnet(rebuild_feature).reshape(B, M, 3, -1)    # B M 3 S
+        relative_xyz = self.foldingnet(rebuild_feature)
+        label = relative_xyz.reshape(B, M, relative_xyz.size(1)*relative_xyz.size(2))
+        label = self.CNN(label)
+        relative_xyz = relative_xyz.reshape(B, M, 3, -1)    # B M 3 S
+        #print(">>>>>>>>")
+        #print(label)
         rebuild_points = (relative_xyz + coarse_point_cloud.unsqueeze(-1)).transpose(2,3).reshape(B, -1, 3)  # B N 3
 
         # NOTE: fc
@@ -117,7 +173,7 @@ class PoinTr(nn.Module):
         inp_sparse = fps(xyz, self.num_query)
         coarse_point_cloud = torch.cat([coarse_point_cloud, inp_sparse], dim=1).contiguous()
         rebuild_points = torch.cat([rebuild_points, xyz],dim=1).contiguous()
-
-        ret = (coarse_point_cloud, rebuild_points)
+        #print(coarse_point_cloud.size())
+        #print(rebuild_points.size())
+        ret = (coarse_point_cloud, rebuild_points,label)
         return ret
-
