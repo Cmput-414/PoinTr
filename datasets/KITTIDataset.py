4ecbe24d412d37b9de108219fc1bbcf3e8a466c6
jackie174 <57372321+jackie174@users.noreply.github.com>
2022-12-16 17:17:17 -0700

    update 8

--- datasets/KITTIDataset.py
+++ datasets/KITTIDataset.py
@@ -71,6 +71,6 @@ class KITTI(data.Dataset):
 
         if self.transforms is not None:
             data = self.transforms(data)
-        classV = np.zeros(self.clst)
+        classV = np.zeros(8)
         classV[2] = 1
         return  sample['taxonomy_id'], sample['model_id'], data['partial_cloud'], classV
